d34591ec3a21d6cc7e4394c7f76b73702db22871
P411max999 <jaccybbq@gmail.com>
2023-08-12 11:56:43 +0800

    solve conflicts

--- javascript/solid-triangle.js
+++ javascript/solid-triangle.js
@@ -6,24 +6,14 @@ class SolidTriangle extends PaintFunction {
     this.coordinates = []; // use array to store the coordinates
     this.selectcolor = document.getElementById("favcolorBorder").value;
   }
-
   onMouseDown(coord, event) {
-<<<<<<< HEAD
-    this.contextReal.fillStyle = `${this.selectcolor}`;
-=======
     this.contextReal.fillStyle = `${selectcolor.value}`;
->>>>>>> b0c1baad2efacaf455d073e910ab4eec1ade3a10
     this.coordinates.unshift(coord); // sotre the first coordinate，用push每次只能取最原始一個click坐標數，unshift可以更新下一個點擊坐標數
     this.selectcolor = document.getElementById("favcolorBorder").value;
   }
-
   onDragging(coord, event) {
     this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-<<<<<<< HEAD
-    this.contextDraft.fillStyle = `${this.selectcolor}`;
-=======
     this.contextDraft.fillStyle = `${selectcolor.value}`;
->>>>>>> b0c1baad2efacaf455d073e910ab4eec1ade3a10
     const startPoint = this.coordinates[0];
     const endPoint = coord;
     // //  計算中點的坐標[(x1+x2)/2,(y1+y2)/2]
@@ -44,7 +34,7 @@ class SolidTriangle extends PaintFunction {
 
   onMouseUp(coord) {
     this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-    this.contextReal.fillStyle = `${this.selectcolor}`;
+    this.contextReal.fillStyle = `${selectcolor.value}`;
     const startPoint = this.coordinates[0];
     const endPoint = coord;
     // const midPoint = [
